058a0b6f21e5824ee38cf4e7cecce0714d07a3f7
juyonghyeon <jyh928578@gmail.com>
2025-07-18 10:38:38 +0900

    07/18

--- src/main/java/org/koreait/mypage/controllers/MypageController.java
+++ src/main/java/org/koreait/mypage/controllers/MypageController.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/mypage")
 @ApplyCommonController
+@RequestMapping("/mypage")
 @RequiredArgsConstructor
 public class MypageController {
     private final Utils utils;
